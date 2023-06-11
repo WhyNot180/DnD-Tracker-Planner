fa147d07397bd2f7ba4d6710be8866533f2d8f60
Bence Szucs <bence.szucs200625@gmail.com>
2023-06-10 20:16:36 -0400

    Adds neo4j driver campaign query

--- Dungeons and Dragons Tracker-Planner/Dungeons and Dragons Tracker-Planner/MainWindow.xaml.cs
+++ Dungeons and Dragons Tracker-Planner/Dungeons and Dragons Tracker-Planner/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Neo4j.Driver;
 
 namespace Dungeons_and_Dragons_Tracker_Planner
 {
@@ -20,9 +21,68 @@ namespace Dungeons_and_Dragons_Tracker_Planner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IDriver _driver;
+
+        public void GraphDriver_Init(string uri)
+        {
+            _driver = GraphDatabase.Driver(uri);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            GraphDriver_Init("bolt://localhost:7687");
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+
+        private void SearchText_Changed(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(SearchText.Text);
+        }
+
+        private async void Campaign_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Campaign_Btn_Grid.Visibility = Visibility.Collapsed;
+            Adventure_Btn_Grid.Visibility = Visibility.Collapsed;
+            NPCs_Btn_Grid.Visibility = Visibility.Collapsed;
+            Encounters_Btn_Grid.Visibility = Visibility.Collapsed;
+
+            Back_Btn_Grid.Visibility = Visibility.Visible;
+            SearchText_Grid.Visibility = Visibility.Visible;
+
+            var session = _driver.AsyncSession();
+            var campaigns = await session.ExecuteReadAsync(
+                async tx =>
+                {
+
+                    var resultsList = new List<string>();
+
+                    var reader = await tx.RunAsync(
+                        "MATCH (c:Campaign) RETURN c.name");
+                    
+                    while (await reader.FetchAsync())
+                    {
+                        resultsList.Add(reader.Current[0].ToString());
+                    }
+
+                    return resultsList;
+                });
+            
+            foreach (var campaign in campaigns )
+            {
+                Grid grid = new Grid();
+                Button button = new Button();
+                st_pnl.Children.Add(grid);
+                grid.Children.Add(button);
+                grid.Style = (Style) st_pnl.FindResource("sidebar_grids"); 
+                button.Style = (Style) st_pnl.FindResource("sidebar_buttons");
+                button.Content = campaign;
+            }
+            
         }
     }
 }
--- Dungeons and Dragons Tracker-Planner/Dungeons and Dragons Tracker-Planner/obj/Debug/Dungeons and Dragons Tracker-Planner_Content.g.i.cs
+++ Dungeons and Dragons Tracker-Planner/Dungeons and Dragons Tracker-Planner/obj/Debug/Dungeons and Dragons Tracker-Planner_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/sidebar_button_border.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/search_button.png")]
 
 
